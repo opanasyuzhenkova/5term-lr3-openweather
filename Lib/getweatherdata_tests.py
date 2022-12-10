a7df7ffb8f9b1e0d16edf84502493fd5b4fdc6ca
opanasyuzhenkova <panasyuzhenkova@gmail.com>
2022-12-10 14:50:42 +0300

    fixes

--- Lib/getweatherdata_tests.py
+++ Lib/getweatherdata_tests.py
@@ -5,49 +5,49 @@ key = ''
 
 
 def test_without_key():
-  assert getweatherdata.get_weather_data(
-      "moscow") is None, \
-      " Для получения данных необходимо задать значение для api_key "
+    assert getweatherdata.get_weather_data(
+        "moscow") is None, \
+        " Для получения данных необходимо задать значение для api_key "
 
 
 def test_in_riga():
-  assert getweatherdata.get_weather_data("Riga",
-                                         api_key=key) is not None, \
-      " Type of response is not none while using the key "
+    assert getweatherdata.get_weather_data("Riga",
+                                           api_key=key) is not None, \
+        " Type of response is not none while using the key "
 
 
 def test_type_of_res():
-  assert type(getweatherdata.get_weather_data("Riga",
-                                              api_key=key)) is str, \
-      " Type of response is not none while using the key "
+    assert type(getweatherdata.get_weather_data("Riga",
+                                                api_key=key)) is str, \
+        " Type of response is not none while using the key "
 
 
 def test_args_error():
-  assert getweatherdata.get_weather_data(
-      '') is None, \
-      " There should be one positional argument: 'city' and one keyword argument 'key_arg'"
+    assert getweatherdata.get_weather_data(
+        '') is None, \
+        " There should be one positional argument: 'city' and one keyword argument 'key_arg'"
 
 
 def test_pos_arg_error():
-  assert getweatherdata.get_weather_data('',
-                                         api_key=key) is None, \
-      " There should be one positional argument: 'city' "
+    assert getweatherdata.get_weather_data('',
+                                           api_key=key) is None, \
+        " There should be one positional argument: 'city' "
 
 
 def test_coords_dim():
-  import json
+    import json
 
-  assert len(
-      json.loads(getweatherdata.get_weather_data('Riga', api_key=key)).get('coord')) == 2, \
-      " Dimension is 2: lon and lat "
+    assert len(
+        json.loads(getweatherdata.get_weather_data('Riga', api_key=key)).get('coord')) == 2, \
+        " Dimension is 2: lon and lat "
 
 
 def test_temp_type():
-  import json
+    import json
 
-  assert type(json.loads(getweatherdata.get_weather_data('Riga', api_key=key)).get(
-      'feels_like')) is float, \
-      " Error with type of feels_like attribute "
+    assert type(json.loads(getweatherdata.get_weather_data('Riga', api_key=key)).get(
+        'feels_like')) is float, \
+        " Error with type of feels_like attribute "
 
 
 inp_params_1 = "city, api_key, expected_country"
@@ -59,11 +59,11 @@ exp_params_countries = [("Chicago", key, 'US'),
 
 @pytest.mark.parametrize(inp_params_1, exp_params_countries)
 def test_countries(city, api_key, expected_country):
-  import json
+    import json
 
-  assert json.loads(getweatherdata.get_weather_data(city, api_key=key)).get('country',
-                                                                            'NoValue') == expected_country, \
-      " Error with country code "
+    assert json.loads(getweatherdata.get_weather_data(city, api_key=key)).get('country',
+                                                                              'NoValue') == expected_country, \
+        " Error with country code "
 
 
 #
@@ -78,7 +78,7 @@ exp_params_timezones = [("Chicago", key, 'UTC-5'),
 
 @pytest.mark.parametrize(inp_params_2, exp_params_timezones)
 def test_utc_time(city, api_key, expected_time):
-  import json
-  assert json.loads(getweatherdata.get_weather_data(city, api_key=key)).get('timezone',
-                                                                            'NoValue') == expected_time, \
-      " Error with timezone "
+    import json
+    assert json.loads(getweatherdata.get_weather_data(city, api_key=key)).get('timezone',
+                                                                              'NoValue') == expected_time, \
+        " Error with timezone "
--- Lib/main.py
+++ Lib/main.py
@@ -1,10 +1,11 @@
 from owm_key import owm_api_key
 from getweatherdata import get_weather_data
-#import os
 
-#owm_api_key = os.environ['owm_api_key'] repl
+# import os
+# owm_api_key = os.environ['owm_api_key'] repl
+
 
 if __name__ == '__main__':
-  get_weather_data('Moscow', api_key=owm_api_key)
-  get_weather_data('Chicago', api_key=owm_api_key)
-  get_weather_data('Sankt-Peterburg', api_key=owm_api_key)
+    get_weather_data('Moscow', api_key=owm_api_key)
+    get_weather_data('Chicago', api_key=owm_api_key)
+    get_weather_data('Sankt-Peterburg', api_key=owm_api_key)
